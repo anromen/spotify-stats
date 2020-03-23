5f270e033164fdcc1c38af4be6b569f8b7d96404
Angel <migg.ro3@gmail.com>
2020-03-23 10:43:02 -0500

    Static artist column done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="float"></div>
+      <div className="column-3 solid">
+        <h2>SONGS</h2>
+        <ol>
+          <li>
+            <div className="song first">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+          <li>
+            <div className="song">
+              <img src="img/cover.jpg" alt="cover" className="cover" />
+              <h3>El mundo sigue girando</h3>
+              <small>Energía - El Chojin</small>
+            </div>
+          </li>
+        </ol>
+      </div>
+      <div className="column-3">
+        <h2>ARTISTS</h2>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+        <div className="artist">
+          <div className="profile-image"></div>
+          <div className="artist"></div>
+        </div>
+      </div>
+      <div className="column-3 profile"></div>
     </div>
   );
 }
