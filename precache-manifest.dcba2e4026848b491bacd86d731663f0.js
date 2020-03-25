d1b2f96f510d51d22d9e4bf7f69e650896624f23
Angel <migg.ro3@gmail.com>
2020-03-25 12:01:44 -0500

    Updates

---
+++ precache-manifest.dcba2e4026848b491bacd86d731663f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cdc52efabe6c643896b6c1b78bda52",
+    "url": "/spotfify-stats/index.html"
+  },
+  {
+    "revision": "2ec4f092fc00de8a280a",
+    "url": "/spotfify-stats/static/css/main.bcf4aa63.chunk.css"
+  },
+  {
+    "revision": "8a18a9ca46f4c8566c8a",
+    "url": "/spotfify-stats/static/js/2.5ebef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotfify-stats/static/js/2.5ebef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec4f092fc00de8a280a",
+    "url": "/spotfify-stats/static/js/main.4e87b984.chunk.js"
+  },
+  {
+    "revision": "f36e39b6ce4232e3eeb1",
+    "url": "/spotfify-stats/static/js/runtime-main.901b44ab.js"
+  }
+]);
